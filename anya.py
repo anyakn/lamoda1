cf282c8a5cfd6f8bf592fa3091428754c66aaeae
anyaknopova <annilik22@gmail.com>
2023-10-15 21:51:10 +0700

    /

--- anya.py
+++ anya.py
@@ -10,7 +10,7 @@ n_out = text.find('товар')
 number = str(text[:n_out])
 print(number)
 
-url1 = 'https://www.lamoda.ru/p/rtlada204601/shoes-philippplein-sapogi/'
+url1 = 'https://www.lamoda.ru/p/mp002xw0rlgj/bags-keddo-sumka/'
 r1 = requests.get(url1)
 text1 = r1.text
 print(text1)
@@ -23,3 +23,13 @@ text1 = text1[country_in+39:]
 country_out = text1.find('"')
 country = text1[:country_out]
 print(country)
+
+discount_in = text1.find('"discount_lamoda_and_loyalty_and_action"')
+if discount_in != -1:
+    text1 = text1[discount_in+41:]
+    discount_out = text1.find(',')
+    discount = text1[:discount_out]
+    print(discount)
+else:
+    discount = 0
+    print(discount)
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import requests
+import math
 
 all_urls = ['https://www.lamoda.ru//p/mp002xg032mz/accs-sela-beysbolka/', 'https://www.lamoda.ru//p/mp002xb01y0v/accs-sela-beysbolka/', 'https://www.lamoda.ru//p/mp002xg032lp/accs-sela-beysbolka/', 'https://www.lamoda.ru//p/mp002xg032ms/accs-sela-beysbolka/', 'https://www.lamoda.ru//p/mp002xc0168x/accs-mothercare-kepka/', 'https://www.lamoda.ru//p/mp002xw0tn8g/accs-befree-beysbolka/', 'https://www.lamoda.ru//p/mp002xw00sfj/accs-befree-beysbolka/', 'https://www.lamoda.ru//p/mp002xw0qezy/accs-befree-beysbolka/', 'https://www.lamoda.ru//p/mp002xw0tn8p/accs-befree-beysbolka/', 'https://www.lamoda.ru//p/mp002xm253to/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xw0sgri/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xb01z1s/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xg034gm/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xg033bp/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xg033az/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xb01xtl/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xb01ya2/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xb01z1r/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xb020g6/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xb01xtt/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xb01y9x/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xw01ams/accs-sela-beysbolka/', 'https://www.lamoda.ru//p/mp002xb01xzn/accs-sela-beysbolka/', 'https://www.lamoda.ru//p/mp002xg033bj/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xb01ish/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xg034gn/accs-gloriajeans-kepka/', 'https://www.lamoda.ru//p/mp002xw0iv0r/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xb01xsl/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xg033bu/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xg033bi/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xb01xsq/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xb01xst/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xb020fx/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xb020g2/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xb01xt4/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xb01xsv/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xb01xty/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xb01z1v/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xb01isk/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xb020g7/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xb01ia4/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xg033bf/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xg02eoy/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xg033bh/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xg02fjw/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xg033b2/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xg033b3/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xg033b8/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xg02ep4/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xg034gr/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xg02ep3/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xg033b9/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xw0iv0w/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xw0iv0o/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xw01alc/accs-sela-beysbolka/', 'https://www.lamoda.ru//p/mp002xw0sgrf/accs-gloriajeans-beysbolka/', 'https://www.lamoda.ru//p/mp002xb01ikl/accs-mothercare-kepka/', 'https://www.lamoda.ru//p/mp002xw0x6d7/accs-befree-beysbolka/', 'https://www.lamoda.ru//p/mp002xw0x6d8/accs-befree-beysbolka/', 'https://www.lamoda.ru//p/mp002xw19m9w/accs-befree-beysbolka/']
 
@@ -15,3 +17,27 @@ df = pd.DataFrame(data)
 
 with open('output.txt', 'w', encoding='utf8') as f_out:
     print(df, file=f_out)
+
+
+query = input('Введите запрос: ')
+query = query.replace(' ', '+')
+query = query.lower()
+
+url = 'https://www.lamoda.ru/catalogsearch/result/?q='+query+'&sort=price_asc'
+r = requests.get(url)
+text = r.text
+
+num_in = text.find('</h2><span class="d-catalog-header__product-counter">')
+text = text[num_in+53:]
+num_out = text.find('товар')
+num = int(text[:num_out])
+
+pages = math.ceil(num/60)
+
+for page in range(pages):
+    url = url + '&page=' + str(page)
+    r = requests.get(url)
+    text = r.text
+
+
+
